b90939cef7bb3bc7187ada2eb240997361508ce2
naveen <naveen.jadala@gmail.com>
2018-11-03 18:27:33 +0530

    error fixed

--- controllers/prolist.ctrl.js
+++ controllers/prolist.ctrl.js
@@ -1,7 +1,7 @@
 var prolist = {
     
     get: function( req, res) {
-        var pro = [{id :1,name:"lisa ann",age:42},{id :2,name:"sofidee",age:35},{id :4git ,name:"mia kalifa",age:25}];
+        var pro = [{id :1,name:"lisa ann",age:42},{id :2,name:"sofidee",age:35},{id :4 ,name:"mia kalifa",age:25}];
         res.json(pro);
     }
 };
